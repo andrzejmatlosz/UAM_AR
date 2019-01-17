7fc3d4520254c831deb1b256de0e6b73d1400b85
Andrzej Matłosz <andrzej.matlosz@gmail.com>
2019-01-17 11:29:30 +0100

    initial commit

---
+++ Assets/CarMoveController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMoveController : MonoBehaviour {
+
+    CacheController cacheController;
+    private bool forward;
+    private bool backwards;
+
+    // Use this for initialization
+    void Start () {
+        cacheController = GetComponent<CacheController>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (forward)
+        {
+            cacheController.car.transform.Translate(Vector3.up * 0.01f);
+        }
+
+        if (backwards)
+        {
+            cacheController.car.transform.Translate(Vector3.down * 0.01f);
+        }
+    }
+
+    public void Stop()
+    {
+        forward = false;
+        backwards = false;
+    }
+
+    public void MoveForward()
+    {
+        forward = true;
+        backwards = false;
+    }
+
+    public void MoveBackwards()
+    {
+        backwards = true;
+        forward = false;
+    }
+}
---
+++ Assets/NewCarController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewCarController : MonoBehaviour {
+
+    public CacheController cacheController;
+
+	// Use this for initialization
+	void Start () {
+        cacheController.car = this.gameObject;
+    }
+}
